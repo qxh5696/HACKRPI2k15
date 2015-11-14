da4da920878b2a2b2d1d8061fef763565c348584
Daniel Roach <dannycr09@hotmail.com>
2015-11-14 16:45:25 -0500

    Added in some model files

---
+++ app/src/main/java/com/example/qadirhaqq/hackrpi2k15/TripAdvisorSuggestion.java
@@ -0,0 +1,12 @@
+package com.example.qadirhaqq.hackrpi2k15;
+
+/**
+ * Created by dcr on 11/14/15.
+ */
+public class TripAdvisorSuggestion {
+
+    private String street;
+    private String name;
+    private double distance;
+
+}

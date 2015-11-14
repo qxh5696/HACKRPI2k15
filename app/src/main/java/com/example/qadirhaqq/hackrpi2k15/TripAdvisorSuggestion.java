5dda6dafcef84b1d0e579929a8478123efeba717
Qadir Haqq <Haqq.Q173@gmail.com>
2015-11-14 17:06:28 -0500

    Removed Google Maps .XML statement and the import statements in TripAdvisonSuggestion.java

--- app/src/main/java/com/example/qadirhaqq/hackrpi2k15/TripAdvisorSuggestion.java
+++ app/src/main/java/com/example/qadirhaqq/hackrpi2k15/TripAdvisorSuggestion.java
@@ -1,5 +1,8 @@
 package com.example.qadirhaqq.hackrpi2k15;
 
+
+import android.app.Activity;
+import android.os.Bundle;
 /**
  * Created by dcr on 11/14/15.
  */
@@ -8,5 +11,9 @@ public class TripAdvisorSuggestion {
     private String street;
     private String name;
     private double distance;
+    private double latitude;
+    private double longitude;
+
+
 
 }
